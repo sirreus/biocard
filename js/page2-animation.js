fb9fd4eb402a619c5ccadf724b173de8189c7d31
sirreus <kirill.egorov@x5.ru>
2021-03-29 12:52:44 +0300

    tried tune css for iOS

---
+++ js/page2-animation.js
@@ -0,0 +1,49 @@
+if ($(window).width() > 760) {
+  var photo1 = document.getElementById("page2-photo1");
+  var scene = new ScrollMagic.Scene({ triggerElement: "#page2-trigger1" })
+    .on("enter", function () {
+      photo1.style.opacity = "1";
+    })
+    .on("leave", function () {
+      photo1.style.opacity = "0";
+    })
+    .addIndicators()
+    .addTo(controller);
+
+  var photo2 = document.getElementById("page2-photo2");
+  var scene = new ScrollMagic.Scene({ triggerElement: "#page2-trigger2" })
+    .on("enter", function () {
+      photo2.style.opacity = "1";
+    })
+    .on("leave", function () {
+      photo2.style.opacity = "0";
+    })
+    .addIndicators()
+    .addTo(controller);
+
+  var photo3 = document.getElementById("page2-photo3");
+  var scene = new ScrollMagic.Scene({ triggerElement: "#page2-trigger3" })
+    .on("enter", function () {
+      photo3.style.opacity = "1";
+    })
+    .on("leave", function () {
+      photo3.style.opacity = "0";
+    })
+    .addIndicators()
+    .addTo(controller);
+
+  if ($(window).width() > 1024 && $(window).width() < 1440) {
+    var page2TextUpDuration = "85%";
+  } else if ($(window).width() >= 800 && $(window).width() <= 1024) {
+    var page2TextUpDuration = "60%";
+  } else {
+    var page2TextUpDuration = "73%";
+  }
+  var scene = new ScrollMagic.Scene({
+    triggerElement: "#page2-trigger0",
+    duration: page2TextUpDuration,
+  })
+    .setPin("#page2-title-wrapper")
+    .addIndicators({ name: "text up" })
+    .addTo(controller);
+}
---
+++ js/page3-animation.js
@@ -0,0 +1,61 @@
+if ($(window).width() >= 1600) {
+  var page3Duration = "180%";
+  var page3Offset = "60%";
+  var page3ScrollOffset = page3Offset;
+} else if ($(window).width() >= 1420 && $(window).width() < 1600) {
+  var page3Duration = "150%";
+  var page3Offset = "50%";
+  var page3ScrollOffset = page3Offset;
+} else if ($(window).width() > 1024 && $(window).width() < 1420) {
+  var page3Duration = "150%";
+  var page3Offset = "0%";
+  var page3ScrollOffset = page3Offset;
+} else if ($(window).width() >= 800 && $(window).width() <= 1024) {
+  var page3Duration = "130%";
+  var page3Offset = "0%";
+  var page3ScrollOffset = "0%";
+} else if ($(window).width() < 800) {
+  var page3Duration = "90%";
+  var page3Offset = "30%";
+  var page3ScrollOffset = page3Offset;
+}
+
+var content = document.getElementById("page3-content-wrapper");
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#page3-trigger1",
+  duration: page3Duration,
+  offset: page3Offset,
+})
+  .setPin(content)
+  .addIndicators({ name: "pin content" })
+  .addTo(controller);
+
+var tweenLeft = TweenMax.to(
+  "#page3-title-wrapper1",
+  8.5,
+  { className: "+=fishRightFull", ease: Power2.easeOut },
+  0
+);
+var scene2 = new ScrollMagic.Scene({
+  triggerElement: "#page3-trigger2",
+  duration: page3Duration,
+  offset: page3ScrollOffset,
+})
+  .setTween(tweenLeft)
+  .addIndicators({ name: "go to right" })
+  .addTo(controller);
+
+var tweenRight = TweenMax.to(
+  "#page3-title-wrapper2",
+  8.5,
+  { className: "+=fishLeftFull", ease: Power2.easeOut },
+  0
+);
+var scene3 = new ScrollMagic.Scene({
+  triggerElement: "#page3-trigger2",
+  duration: page3Duration,
+  offset: page3ScrollOffset,
+})
+  .setTween(tweenRight)
+  .addIndicators({ name: "go to left" })
+  .addTo(controller);
