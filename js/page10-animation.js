5e2baf1c4ddc705f63c747f712ec8965b4f7d780
sirreus <kirill.egorov@x5.ru>
2021-04-01 10:54:10 +0300

    tune blue pages anim for W > 1680

--- js/page10-animation.js
+++ js/page10-animation.js
@@ -15,29 +15,21 @@ var scene1 = new ScrollMagic.Scene({
   .addIndicators({ name: "darked" })
   .addTo(controller);
 
-var text = TweenMax.to("#page10-text", 1, { className: "+=__visible" });
-var scene1 = new ScrollMagic.Scene({
-  triggerElement: "#page10-trigger1",
-  duration: "35%",
-  offset: "40%",
-})
-  .setTween(text)
-  .addIndicators({ name: "show text" })
-  .addTo(controller);
-
 var textBlock = document.getElementById("page10-text-wrapper");
-if ($(window).width() < 800) {
-  var page7PinDuration = "80%";
-  var page7PinOffset = "40%";
-} else {
-  var page7PinDuration = "40%";
-  var page7PinOffset = "80%";
-}
+var page7PinDuration = "140%";
 var scene2 = new ScrollMagic.Scene({
-  triggerElement: "#page10-trigger1",
+  triggerElement: "#page10-trigger2",
   duration: page7PinDuration,
-  offset: page7PinOffset,
 })
   .setPin(textBlock)
   .addIndicators({ name: "pin text" })
+  .addTo(controller);
+
+var text = TweenMax.to("#page10-text", 1, { className: "+=__visible" });
+var scene1 = new ScrollMagic.Scene({
+  triggerElement: "#page10-trigger3",
+  duration: "35%",
+})
+  .setTween(text)
+  .addIndicators({ name: "show text" })
   .addTo(controller);
