e14bcce1e1521381b04a3c349906e0f0f677121b
sirreus <kirill.egorov@x5.ru>
2021-04-08 16:20:44 +0300

    tune page2 anim.js

--- js/page2-animation.js
+++ js/page2-animation.js
@@ -34,18 +34,21 @@ if ($(window).width() > 760) {
     .addIndicators()
     .addTo(controller);
 
+  var windowHeight = $(window).height();
+  var onePercentHeight = windowHeight / 100;
+
   if ($(window).width() >= 1440 && $(window).width() <= 1680) {
-    var page2TextUpDuration = "75%";
+    var page2TextUpDuration = onePercentHeight * 80;
   } else if ($(window).width() >= 1024 && $(window).width() < 1440) {
-    var page2TextUpDuration = "65%";
+    var page2TextUpDuration = onePercentHeight * 75;
   } else if ($(window).width() >= 800 && $(window).width() < 1024) {
-    var page2TextUpDuration = "60%";
+    var page2TextUpDuration = onePercentHeight * 60;
   } else {
-    var page2TextUpDuration = "85%";
+    var page2TextUpDuration = onePercentHeight * 85;
   }
   var scene = new ScrollMagic.Scene({
     triggerElement: "#page2-trigger0",
-    duration: page2TextUpDuration,
+    duration: `${page2TextUpDuration}`,
   })
     .setPin("#page2-title-wrapper")
     .addIndicators({ name: "text up" })
