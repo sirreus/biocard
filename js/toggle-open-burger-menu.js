9a16b5a65f1924f2022d167598a6249858ec5ef8
sirreus <kirill.egorov@x5.ru>
2021-04-08 18:41:58 +0300

    fixed change logo color on hide menu

--- js/toggle-open-burger-menu.js
+++ js/toggle-open-burger-menu.js
@@ -1,29 +1,34 @@
 $(document).ready(function () {
+  // объявляем глобальную переменную
+  var modifier = null;
+
   $("#menu-button").click(function () {
-    if ($(this).hasClass("__to-open-white")) {
-      $(this).toggleClass("__to-open-white");
+    // грабим элемент на тему классов
+    var buttonClasses = $(this).attr("class").split(" ");
 
-      if ($(this).hasClass("__to-open-white")) {
-        $("#menu").slideUp("slow", function () {
-          $("#menu").css("display", "none");
-        });
-        $(".logo").attr("src", "./assets/logo-white.svg");
-      } else {
-        $("#menu").slideDown("slow", function () {});
-        $(".logo").attr("src", "./assets/logo-black.svg");
-      }
+    /**
+     * проверяем если у кнопки больше чем 1 класс,
+     * т.е. есть модификатор
+     * записываем в глобальную переменную этот модификатор
+     */
+    if (buttonClasses.length > 1) {
+      modifier = buttonClasses[1];
+    }
+    // если нет модификатора, то ничего не делаем
+
+    $(this).toggleClass(modifier);
+
+    if ($(this).hasClass(modifier)) {
+      $("#menu").slideUp("slow", function () {
+        $("#menu").css("display", "none");
+      });
+      // вырезаем цвет из модификатора и используем для лого
+      var color = modifier.split("__to-open-")[1];
+      $(".logo").attr("src", `./assets/logo-${color}.svg`);
     } else {
-      $(this).toggleClass("__to-open-black");
-
-      if ($(this).hasClass("__to-open-black")) {
-        $("#menu").slideUp("slow", function () {
-          $("#menu").css("display", "none");
-        });
-        $(".logo").attr("src", "./assets/logo-black.svg");
-      } else {
-        $("#menu").slideDown("slow", function () {});
-        $(".logo").attr("src", "./assets/logo-black.svg");
-      }
+      $("#menu").slideDown("slow", function () {});
+      // для меню лого всегда ставим черное лого
+      $(".logo").attr("src", "./assets/logo-black.svg");
     }
   });
 });
